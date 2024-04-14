33ca6cf6b50010546f48a6e31345bf7c00a504bc
Max Cai <maxcai.paloalto@gmail.com>
2024-04-14 09:12:20 -0700

    use the polymorphic insert(ResourceRecord), maybe a bad idea?

--- src/main/java/ax/xz/max/dns/Database.java
+++ src/main/java/ax/xz/max/dns/Database.java
@@ -7,9 +7,9 @@ public class Database {
 	public static void main(String[] args) {
 		SQLResourceRepository controller = new SQLResourceRepository();
 		controller.clear();
-		controller.insertARecord(new ARecord("example.com", new byte[]{127, 0, 0, 1}));
-		controller.insertARecord(new ARecord("example.net", new byte[]{127, 0, 0, 2}));
-		controller.insertARecord(new ARecord("example.org", new byte[]{127, 0, 0, 3}));
+		controller.insert(new ARecord("example.com", new byte[]{127, 0, 0, 1}));
+		controller.insert(new ARecord("example.net", new byte[]{127, 0, 0, 2}));
+		controller.insert(new ARecord("example.org", new byte[]{127, 0, 0, 3}));
 		controller.deleteARecord("example.net");
 		controller.printAll();
 		System.out.println(controller.getARecord("example.com"));
