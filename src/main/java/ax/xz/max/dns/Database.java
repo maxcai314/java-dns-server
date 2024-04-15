e15d7501934746931e0e1b592466417ed58bfd26
Max Cai <maxcai.paloalto@gmail.com>
2024-04-14 21:37:53 -0700

    test gettting mulitple

--- src/main/java/ax/xz/max/dns/Database.java
+++ src/main/java/ax/xz/max/dns/Database.java
@@ -18,10 +18,15 @@ public class Database {
 
 		var com = new ARecord(new DomainName("example.com"), 10, Inet4Address.ofLiteral("127.0.0.1"));
 		var net = new NSRecord(new DomainName("example.net"), 60, new DomainName("ns.example.net"));
-		var org = new CNameRecord(new DomainName("www.example.org"), 500, new DomainName("example.org"));
+		var wwwOrg = new CNameRecord(new DomainName("www.example.org"), 500, new DomainName("example.org"));
+		var org = new ARecord(new DomainName("example.org"), 500, Inet4Address.ofLiteral("127.0.0.1"));
+		var org2 = new ARecord(new DomainName("example.org"), 500, Inet4Address.ofLiteral("127.0.0.2"));
+
 		controller.insert(com);
 		controller.insert(net);
+		controller.insert(wwwOrg);
 		controller.insert(org);
+		controller.insert(org2);
 		controller.delete(net); // not implemented!
 
 		System.out.println("printing all:");
@@ -29,6 +34,6 @@ public class Database {
 
 		System.out.println("\nprinting all by name:");
 		System.out.println(controller.getAllByName(new DomainName("example.com")));
-		System.out.println(controller.getAllByName(new DomainName("example.net"))); // empty
+		System.out.println(controller.getAllByName(new DomainName("example.org"))); // empty
 	}
 }
